cdf11b9b5cbb93cb4c50b11565cbad441fda5d60
bblair321 <brady@BradysPC.>
2025-05-15 18:07:26 -0700

    completed all tests

--- index.js
+++ index.js
@@ -1,6 +1,39 @@
+require('datejs');
 
+const usernames1 = [
+  "coolCat123",
+  "sunshine_89",
+  "bluePenguin",
+  "fastRunner",
+  "nightOwl",
+];
 
+const usernames2 = [
+  "alphaWolf",
+  "pixelPirate",
+  "neonNinja",
+  "lunarLight",
+  "cryptoQueen",
+];
 
-module.exports = {
-  ...(typeof combineUsers !== 'undefined' && { combineUsers })
-};
+const usernames3 = [
+  "gamer_guy",
+  "bookworm42",
+  "musicLover",
+  "travelBug",
+  "coffeeAddict",
+];
+
+function combineUsers(...usernamesArrays) {
+  const combinedObject = { users: [] };
+
+  for (const arr of usernamesArrays) {
+    combinedObject.users = [...combinedObject.users, ...arr];
+  }
+
+  combinedObject.merge_date = new Date().toString('M/d/yyyy');
+
+  return combinedObject;
+}
+
+module.exports = { combineUsers };
